5a1d97b3d92aafdc5556edf1d808aba593be7881
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2021-05-25 08:25:46 +0530

    add models for users and tasks

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,9 @@
 // CRUD operations -> create, read, update, delete
 
-const { MongoClient, ObjectID } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb")
 
-const connectionURL = "mongodb://127.0.0.1:27017"; // don't use localhost:27017
-const databaseName = "task-manager";
+const connectionURL = "mongodb://127.0.0.1:27017" // don't use localhost:27017
+const databaseName = "task-manager"
 
 MongoClient.connect(
   connectionURL, // connection url
@@ -12,20 +12,20 @@ MongoClient.connect(
   },
   (error, client) => {
     if (error) {
-      return console.log("Unable to connect to database");
+      return console.log("Unable to connect to database")
     }
-    const db = client.db(databaseName);
-    const users = db.collection("users");
-    const tasks = db.collection("tasks");
+    const db = client.db(databaseName)
+    const users = db.collection("users")
+    const tasks = db.collection("tasks")
     tasks
       .deleteOne({
         description: "Course to be complted By May 27",
       })
       .then((res) => {
-        console.log(res.deletedCount);
+        console.log(res.deletedCount)
       })
       .catch((err) => {
-        console.log(err);
-      });
+        console.log(err)
+      })
   }
-);
+)
