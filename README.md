db7ec1bfa52655149c8b7fffd042c2380b30dc13
Manisrinivasa1999 <manisrinivasa1999@gmail.com>
2021-05-25 20:23:18 +0530

    seperate models and add express

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - connect to database, CRUD opreations(check mongodb.js file)
 - ObjectId - Globally unique Identifiers
 - npm i mongoose (validation, authentication)
+- REST API's - representational state transfer application programming interface
+  - Create - POST /tasks
+  - Read - GET /tasks, GET /tasks/:id 
+  - Update - PATCH /tasks/:id
+  - Delete - DELETE /tasks/:id
---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+const User = new mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(val) {
+      if (val.toLowerCase().includes("password")) {
+        throw new Error("Password cannot contain 'password'")
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(val) {
+      if (!validator.isEmail(val)) {
+        throw new Error("Email is invalid")
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(val) {
+      if (val < 0) {
+        throw new Error("Age must be a positive number")
+      }
+    },
+  },
+})
+
+module.exports = User
